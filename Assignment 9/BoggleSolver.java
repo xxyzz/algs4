44da1406b5519f714d38f0da0de43dd53a8cd0b1
xxyzz <elephantpolar@gmail.com>
2018-07-07 23:23:13 +0800

    past the final test

--- Assignment 9/BoggleSolver.java
+++ Assignment 9/BoggleSolver.java
@@ -10,39 +10,38 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Bag;
-import edu.princeton.cs.algs4.Stopwatch;
+// import edu.princeton.cs.algs4.Stopwatch;
 
 public class BoggleSolver {
     private static final int R = 26;        // A - Z
     private Bag<String> wordBag;
+    private Bag<Node> addedNodes;
     private boolean[] marked;
     private char[] letters;
     private int cols;
     private int rows;
     private Node root;      // root of trie
-    private Node lastNode;
-    private String lastPrefix;
+    private int lastPrefixL;
 
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
         if (dictionary == null) throw new IllegalArgumentException("Argument is null");
-        String[] copy = dictionary.clone(); // defensive copy
-        for (int i = 0; i < copy.length; i++) {
-            put(copy[i], 0);
+        for (int i = 0; i < dictionary.length; i++) {
+            put(dictionary[i]);
         }
     }
 
     // R-way trie node
     private static class Node {
-        private int val = -1;
+        private int val;
         private Node[] next = new Node[R];
     }
 
     private int nonrecursiveGet(String key) {
-        if (root == null) return -1;
+        if (root == null) return 0;
         Node x = nonrecursiveGet(root, key);
-        if (x == null) return -1;
+        if (x == null) return 0;
         return x.val;
     }
 
@@ -54,19 +53,19 @@ public class BoggleSolver {
         return x;
     }
 
-    private void put(String key, int val) {
+    private void put(String key) {
         if (key == null) throw new IllegalArgumentException("first argument to put() is null");
-        else root = put(root, key, val, 0);
+        else root = put(root, key, 0);
     }
 
-    private Node put(Node x, String key, int val, int d) {
+    private Node put(Node x, String key, int d) {
         if (x == null) x = new Node();
         if (d == key.length()) {
-            x.val = val;
+            x.val = 1;
             return x;
         }
         char c = key.charAt(d);
-        x.next[c - 'A'] = put(x.next[c - 'A'], key, val, d + 1);
+        x.next[c - 'A'] = put(x.next[c - 'A'], key, d + 1);
         return x;
     }
 
@@ -75,24 +74,30 @@ public class BoggleSolver {
         if (board == null) throw new IllegalArgumentException("Argument is null");
         // reset
         wordBag = new Bag<String>();
+        addedNodes = new Bag<Node>();
         cols = board.cols();
         rows = board.rows();
         marked = new boolean[cols * rows];
         letters = new char[cols * rows];
-        lastNode = root;
-        lastPrefix = "";
+        lastPrefixL = 0;
+
         for (int i = 0; i < cols * rows; i++) {
             letters[i] = board.getLetter(i / cols, i % cols);
         }
 
         for (int i = 0; i < cols * rows; i++) {
-            dfs(i, "");
+            dfs(root, i, "");
+        }
+
+        // reset
+        for (Node node : addedNodes) {
+            node.val = 1;
         }
         return wordBag;
     }
 
     // depth-first search
-    private void dfs(int i, String word) {
+    private void dfs(Node node, int i, String word) {
         if (marked[i]) return;
 
         char letter = letters[i];
@@ -100,45 +105,44 @@ public class BoggleSolver {
         else word += letter;
 
         int wordL = word.length();
-        Node x;
 
-        if (wordL - lastPrefix.length() == 2) {
-            x = nonrecursiveGet(lastNode, "QU");
+        if (wordL > 1 && word.substring(wordL - 2).equals("QU")) {
+            node = nonrecursiveGet(node, "QU");
         }
-        else if (wordL > 1 && lastPrefix.equals(word.substring(0, wordL - 1))) {
-            x = nonrecursiveGet(lastNode, word.substring(wordL - 1));
+        else if (wordL - lastPrefixL == 1) {
+            node = node.next[word.charAt(wordL - 1) - 'A'];
         }
-        else x = nonrecursiveGet(root, word);
+        else node = nonrecursiveGet(root, word);
 
-        if (x != null) {
-            lastNode = x;
-            lastPrefix = word;
+        if (node != null) {
+            lastPrefixL = wordL;
         }
         else return;
 
-        if (word.length() > 2 && x.val == 0) {
+        if (word.length() > 2 && node.val == 1) {
             wordBag.add(word);
-            x.val = 1; // mark added, avoid adding repeated word
+            addedNodes.add(node);
+            node.val = 2; // mark added, avoid adding repeated word
         }
 
-         // mark as visited only after dictionary has words with this prefix
+        // mark as visited only after dictionary has words with this prefix
         marked[i] = true;
 
         // down
-        if (i / cols < rows - 1) dfs(i + cols, word);
+        if (i / cols < rows - 1) dfs(node, i + cols, word);
         // up
-        if (i - cols >= 0) dfs(i - cols, word);
+        if (i - cols >= 0) dfs(node, i - cols, word);
 
         if (i % cols != 0) {
-            dfs(i - 1, word); // left
-            if (i / cols != 0) dfs(i - cols - 1, word); // up left
-            if (i / cols != rows - 1) dfs(i + cols - 1, word); // down left
+            dfs(node, i - 1, word); // left
+            if (i / cols != 0) dfs(node, i - cols - 1, word); // up left
+            if (i / cols != rows - 1) dfs(node, i + cols - 1, word); // down left
         }
 
         if (i % cols < cols - 1) {
-            dfs(i + 1, word); // right
-            if (i / cols != 0) dfs(i - cols + 1, word); // up right
-            if (i / cols != rows - 1) dfs(i + cols + 1, word); // down right
+            dfs(node, i + 1, word); // right
+            if (i / cols != 0) dfs(node, i - cols + 1, word); // up right
+            if (i / cols != rows - 1) dfs(node, i + cols + 1, word); // down right
         }
 
         // change to not visited for another path
@@ -151,7 +155,7 @@ public class BoggleSolver {
         if (word == null) throw new IllegalArgumentException("Argument is null");
         int wordLength = word.length();
         if (wordLength < 3) return 0;
-        if (nonrecursiveGet(word) != -1) {
+        if (nonrecursiveGet(word) != 0) {
             if (wordLength >= 8) return 11;
             else if (wordLength == 7) return 5;
             else if (wordLength == 6) return 3;
@@ -165,33 +169,27 @@ public class BoggleSolver {
     public static void main(String[] args) {
         In in = new In(args[0]);
         String[] dictionary = in.readAllStrings();
-        BoggleBoard board = new BoggleBoard(args[1]);
-        BoggleSolver solver = new BoggleSolver(dictionary);
-        int score = 0, count = 0;
-        for (String word : solver.getAllValidWords(board)) {
-            StdOut.println(word);
-            score += solver.scoreOf(word);
-            count++;
-        }
-        StdOut.println("Score = " + score + " count: " + count);
-        BoggleBoard board2 = new BoggleBoard(args[2]);
-        score = 0;
-        count = 0;
-        for (String word : solver.getAllValidWords(board2)) {
-            StdOut.println(word);
-            score += solver.scoreOf(word);
-            count++;
-        }
-        StdOut.println("Score = " + score + " count: " + count);
-        // In in = new In(args[0]);
-        // String[] dictionary = in.readAllStrings();
         // BoggleBoard board;
-        // if (args.length == 2) board = new BoggleBoard(args[1]);
+        // BoggleBoard board2;
+        // if (args.length == 3) {
+        // board = new BoggleBoard(args[1]);
+        // board2 = new BoggleBoard(args[2]);
+        // }
         // else board = new BoggleBoard();
-        // BoggleSolver solver = new BoggleSolver(dictionary);
+        BoggleSolver solver = new BoggleSolver(dictionary);
         // int score = 0, count = 0;
-        // Stopwatch stopwatch = new Stopwatch();
+        // // Stopwatch stopwatch = new Stopwatch();
         // for (String word : solver.getAllValidWords(board)) {
+        //     StdOut.println(word);
+        //     score += solver.scoreOf(word);
+        //     count++;
+        // }
+        // StdOut.println("Score = " + score + " count: " + count);
+        StdOut.println("Score of live " + solver.scoreOf("LIVE"));
+        // StdOut.println("Time for solver: " + Double.toString(stopwatch.elapsedTime()));
+        // count = 0;
+        // score = 0;
+        // for (String word : solver.getAllValidWords(board2)) {
         //     StdOut.println(word);
         //     score += solver.scoreOf(word);
         //     count++;
