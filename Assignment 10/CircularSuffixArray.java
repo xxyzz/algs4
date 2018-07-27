7a667c0d3c2399a0d649ae1a21042515b5002949
xxyzz <elephantpolar@gmail.com>
2018-07-27 23:27:31 +0800

    not copy string

--- Assignment 10/CircularSuffixArray.java
+++ Assignment 10/CircularSuffixArray.java
@@ -14,61 +14,59 @@ public class CircularSuffixArray {
     // length of input string
     private final int n;
     private int[] index;
-    private final String s;
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
         // Corner case
         if (s == null) throw new IllegalArgumentException("Argument is null");
-        this.s = s;
         n = s.length();
         index = new int[n];
         for (int i = 0; i < n; i++) {
             index[i] = i;
         }
-        sort(0, n - 1, 0);
+        sort(s, 0, n - 1, 0);
     }
 
-    private int charAt(int firstIndex, int d) {
+    private int charAt(String s, int firstIndex, int d) {
         if (d < n && d >= 0) return s.charAt((firstIndex + d) % n);
         else return -1;
     }
 
     // thyree-way string quicksort
-    private void sort(int lo, int hi, int d) {
+    private void sort(String s, int lo, int hi, int d) {
         // cutoff to insertion sort for small subarrays
         if (hi <= lo + CUTOFF) {
-            insertion(lo, hi, d);
+            insertion(s, lo, hi, d);
             return;
         }
 
         int lt = lo, gt = hi;
-        int v = charAt(index[lo], d);
+        int v = charAt(s, index[lo], d);
         int i = lo + 1;
         while (i <= gt) {
-            int t = charAt(index[i], d);
+            int t = charAt(s, index[i], d);
             if (t < v) exch(lt++, i++);
             else if (t > v) exch(i, gt--);
             else i++;
         }
         // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi]
-        sort(lo, lt-1, d);
-        if (v >= 0) sort(lt, gt, d+1);
-        sort(gt+1, hi, d);
+        sort(s, lo, lt-1, d);
+        if (v >= 0) sort(s, lt, gt, d+1);
+        sort(s, gt+1, hi, d);
     }
 
     // sort from index[lo] to index[hi], starting at the dth character
-    private void insertion(int lo, int hi, int d) {
+    private void insertion(String s, int lo, int hi, int d) {
         for (int i = lo; i <= hi; i++)
-            for (int j = i; j > lo && less(j, j - 1, d); j--)
+            for (int j = i; j > lo && less(s, j, j - 1, d); j--)
                 exch(j, j - 1);
     }
 
     // is v less than w, starting at character d
-    private boolean less(int v, int w, int d) {
+    private boolean less(String s, int v, int w, int d) {
         for (int i = d; i < n; i++) {
-            if (charAt(index(v), d) < charAt(index(w), d)) return true;
-            if (charAt(index(v), d) > charAt(index(w), d)) return false;
+            if (charAt(s, index(v), d) < charAt(s, index(w), d)) return true;
+            if (charAt(s, index(v), d) > charAt(s, index(w), d)) return false;
         }
         return false;
     }
